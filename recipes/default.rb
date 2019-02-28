f9d9ca300cdf362dc8882c61a038458b12fe5ffd
Karl Amrhein <karlamrhein@gmail.com>
2019-02-27 23:59:38 -0700

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+# the following will delete the file if the tag is not present,
+# and will create the file if the tag is set.
+#
+# :create is the default action.
+# we only need to list the non-default action of :delete
+
+file '/tmp/tagfound' do
+  action :delete unless node.tags.include?('test_tag')
+end
